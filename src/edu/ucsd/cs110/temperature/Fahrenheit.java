6a5ae01638939e17de35da75db941d54667ea5fb
d1alexan@ucsd.edu <daviders1998@gmail.com>
2018-10-31 12:46:45 -0700

    fixed fahrenheit error

--- src/edu/ucsd/cs110/temperature/Fahrenheit.java
+++ src/edu/ucsd/cs110/temperature/Fahrenheit.java
@@ -9,18 +9,18 @@ public class Fahrenheit extends Temperature
     @Override
     public Temperature toCelsius() {
         Celsius toReturn = new Celsius(this.value);
-        toReturn.value = (toReturn.value - 32) * (5/9);
+        toReturn.value = (toReturn.value - 32)*((float)5/9);
         return toReturn;
     }
 
     @Override
     public Temperature toFahrenheit() {
-        return null;
+        return this;
     }
 
     public String toString()
     {
         // TODO: Complete this method
-        return "";
+        return String.valueOf(this.value)+ " F";
     }
 }
